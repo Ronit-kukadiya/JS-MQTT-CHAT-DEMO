33d086b16342443f0c4ebd61ab9a3013633b4b5c
Ronit <ronitkukadiya999@gmail.com>
2025-03-29 23:24:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const options = {
 };
 // THE OPTIONS ARE NOT USED IN THIS PROJECT
 
-const client = mqtt.connect("wss://broker.hivemq.com:8884/mqtt"); //using hivemq
+const client = mqtt.connect("wss://broker.emqx.io:8084/mqtt"); //using hivemq
 //NOTE: This is not a secure way of using a broker, people using the same topic name on the same broker(here hivemq) will be able to see each other's messages
 //if you want to improve it, try making your own broker.
 
