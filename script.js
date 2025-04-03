f428352aed920684e4161f21e464eae06c3199fb
Ronit <ronitkukadiya999@gmail.com>
2025-04-03 15:21:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,14 +14,15 @@ const linkoptions = {
 };
 
 const client = mqtt.connect("wss://broker.emqx.io:8084/mqtt"); //using hivemq
-//NOTE: This is not a secure way of using a broker, people using the same topic name on the same broker(here hivemq) will be able to see each other's messages
+//NOTE: This is not a secure way of using a broker; people using the same topic name on the same broker(here, emqx) will be able to see each other's messages
 //if you want to improve it, try making your own broker.
 
 let WORD = "";
-let sentIgnore = false; //this setIgnore is used to block/ignore the message sent by you from displaying on the received side since the message
-//go to the same topic and this can lead to what you send being displayed on the receive side, that is the nature of Mqtt,
-//that's how it works. this ignore is not the best solution to deal with this as spamming messages/continuous messages being
-// sent can LEAK into the receive side since 'on.message' is async and constantly listens for incoming messages
+let sentIgnore = false; //this setIgnore is used to block/ignore the message sent by you from displaying on the received side, since the message
+//Go to the same topic, and this can lead to what you send being displayed on the receive side, which is the nature of MQTT.
+//that's how it works. This ignore is not the best solution to deal with this, as spamming messages/continuous messages are being
+//sent can LEAK into the receive side since 'on.message' is async and constantly listens for incoming messages
+//Having a client-ID-based approach is better
 
 let msg = document.getElementById("msg");
 let room = document.getElementById("room");
@@ -106,7 +107,7 @@ function sendMessage() {
   if (WORD === "") {
     swal({
       title: "",
-      text: "join a room first to send message!",
+      text: "Join a room first to send a message!",
       icon: "info",
       button: {
         text: "GOT IT!",
